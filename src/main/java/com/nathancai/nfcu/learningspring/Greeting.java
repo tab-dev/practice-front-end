c8b03bd1aee6a313c602791a034507b3f10a61f3
Nathan Cai <nc5sfu@virginia.edu>
2021-06-22 07:20:19 -0700

    Hard coded greeting with templates

---
+++ src/main/java/com/nathancai/nfcu/learningspring/Greeting.java
@@ -0,0 +1,13 @@
+package com.nathancai.nfcu.learningspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Greeting {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Greeting.class, args);
+	}
+
+}
