c0bc64e60c2fd963c4b9760e5c589b65dfaf7b24
Nathan Cai <nc5sfu@virginia.edu>
2021-06-23 02:29:05 -0700

    Table data still will not show, tried rewriting

---
+++ src/main/java/com/nathancai/nfcu/learningspring/Product.java
@@ -0,0 +1,50 @@
+package com.nathancai.nfcu.learningspring;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+public class Product {
+    @Id
+    private String id;
+
+    @NotNull
+    @Basic(optional=false)
+    private String name;
+
+    private String description;
+
+    @NotNull
+    private BigDecimal price;
+
+    private Integer rating;
+
+    public Product(String id, String name, String description, BigDecimal price, Integer rating) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+}
