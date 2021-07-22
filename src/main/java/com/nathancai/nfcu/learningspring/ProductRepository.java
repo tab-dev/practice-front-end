5959db6170b56eece831a8f4a5a06390f0e50b52
Nathan Cai <nc5sfu@virginia.edu>
2021-07-22 09:51:01 -0400

    Added error handling and also hooked up backend to frontend

--- src/main/java/com/nathancai/nfcu/learningspring/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.nathancai.nfcu.learningspring;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, String> {
-}
--- src/main/java/com/nathancai/nfcu/learningspring/controllers/ProductRestController.java
+++ src/main/java/com/nathancai/nfcu/learningspring/controllers/ProductRestController.java
@@ -1,29 +1,28 @@
-package com.nathancai.nfcu.learningspring.controllers;
-
-import com.nathancai.nfcu.learningspring.Product;
-import com.nathancai.nfcu.learningspring.ProductRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-public class ProductRestController {
-    private final ProductRepository repository;
-
-    ProductRestController(ProductRepository repository) {
-        this.repository = repository;
-    }
-
-    @GetMapping("/products")
-    List<Product> all() {
-        return repository.findAll();
-    }
-
-    @GetMapping("/products/{id}")
-    Product one(@PathVariable String Id) {
-        return repository.findById(Id)
-                .orElseThrow(() -> new ProductNotFoundException(Id));
-    }
-}
+//package com.nathancai.nfcu.learningspring.controllers;
+//
+//import com.nathancai.nfcu.learningspring.Product;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+//@RestController
+//public class ProductRestController {
+//    private final ProductRepository repository;
+//
+//    ProductRestController(ProductRepository repository) {
+//        this.repository = repository;
+//    }
+//
+//    @GetMapping("/products")
+//    List<Product> all() {
+//        return repository.findAll();
+//    }
+//
+//    @GetMapping("/products/{id}")
+//    Product one(@PathVariable String Id) {
+//        return repository.findById(Id)
+//                .orElseThrow(() -> new ProductNotFoundException(Id));
+//    }
+//}
